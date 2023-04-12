36673a3ad893b71f80ce232e954d15707f7c0601
jascolil01 <jayasco246@gmail.com>
2023-04-12 14:12:40 -0400

    added hover feature

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,16 +7,32 @@ export default function About() {
     <div>
       <h1>Meet the Members</h1>
       <div className="about">
-        <div onClick={handleClick}>
+        <div onClick={handleClick} className="test">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
           <img src="https://cdn-icons-png.flaticon.com/512/2884/2884299.png" alt="bass" className="bass" />
         </div>
-        <div onClick={handleClick}>
+        <div onClick={handleClick} className="test">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
           <img src="https://cdn-icons-png.flaticon.com/512/92/92997.png" alt="drums" className="drums" />
         </div>
-        <div onClick={handleClick}>
+        <div onClick={handleClick} className="test">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
           <img src="https://cdn-icons-png.flaticon.com/512/3616/3616721.png" alt="singer" className="singer" />
         </div>
-        <div onClick={handleClick}>
+        <div onClick={handleClick} className="test">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
           <img src="https://i.imgur.com/krcbEPd.png" alt="guitar" className="guitar" />
         </div>
       </div>
