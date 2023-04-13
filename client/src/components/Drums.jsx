82ecc0a40444d06b0982766de960c3d3c7b3d455
jascolil01 <jayasco246@gmail.com>
2023-04-13 15:39:05 -0400

    added template to drums

--- client/src/components/Drums.jsx
+++ client/src/components/Drums.jsx
@@ -1,5 +1,14 @@
 export default function Drums() {
   return (
-    <div>Drums</div>
+    <div>
+      <img src="" alt='profile pic' />
+      <h1>Name</h1>
+      <h2>dhjasd</h2>
+      <h1>Instrument</h1>
+      <h2>Drums</h2>
+      <h1>Contact Information</h1>
+      <h2>Email</h2>
+      <h3><a href='mailto:josh.ascolillo@gmail.com'>fadsasd</a></h3>
+    </div>
   )
 }
