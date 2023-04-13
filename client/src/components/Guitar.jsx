e555b2f900a31c86e451bbb3d23ea8ec3b6d3aa1
jascolil01 <jayasco246@gmail.com>
2023-04-13 15:39:57 -0400

    added template o guiatr

--- client/src/components/Guitar.jsx
+++ client/src/components/Guitar.jsx
@@ -1,5 +1,14 @@
 export default function Guitar() {
   return (
-    <div>Guitar</div>
+    <div>
+      <img src="" alt='profile pic' />
+      <h1>Name</h1>
+      <h2>dhjasd</h2>
+      <h1>Instrument</h1>
+      <h2>Guitar</h2>
+      <h1>Contact Information</h1>
+      <h2>Email</h2>
+      <h3><a href='mailto:josh.ascolillo@gmail.com'>fadsasd</a></h3>
+    </div>
   )
 }
