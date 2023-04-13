fbe9b3fd6e2cb82036e3aca963f4070bbfc23f84
jascolil01 <jayasco246@gmail.com>
2023-04-13 15:19:32 -0400

    added changing clicks

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,47 +2,48 @@ import Drums from "../components/Drums"
 import Bass from "../components/Bass"
 import Singer from "../components/Singer"
 import Guitar from "../components/Guitar"
+import Click from "../components/Click"
 
 export default function About() {
 
   let drum = false
+  let singer = true
 
-  const handleClick = () => {
-    console.log('nice')
+  const handleClick = (x) => {
+    console.log('nice', x)
   }
   return (
     <div>
       <h1>Meet the Members</h1>
       <div className="about">
-        <div onClick={handleClick} className="test">
+        <div onClick={() => handleClick(1)} className="test">
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           <img src="https://cdn-icons-png.flaticon.com/512/2884/2884299.png" alt="bass" className="bass" />
         </div>
-        <div onClick={handleClick} className="test">
+        <div onClick={() => handleClick(2)} className="test">
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           <img src="https://cdn-icons-png.flaticon.com/512/92/92997.png" alt="drums" className="drums" />
         </div>
-        <div onClick={handleClick} className="test">
+        <div onClick={() => handleClick(3)} className="test">
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           <img src="https://cdn-icons-png.flaticon.com/512/3616/3616721.png" alt="singer" className="singer" />
         </div>
-        <div onClick={handleClick} className="test">
+        <div onClick={() => handleClick(4)} className="test">
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           <img src="https://i.imgur.com/krcbEPd.png" alt="guitar" className="guitar" />
         </div>
-        <div>{drum ? <Drums /> : <Click />}</div>
       </div>
     </div>
   )
